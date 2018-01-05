a99d4f0d29897a0acdaf0d879bdd5fb47934a086
Richard Lagrange <richardlagrange1@gmail.com>
2018-01-05 09:53:12 +0100

    add Unregister and WipeContainer methods

--- Sources/Mvvmicro/Dependencies/Container.cs
+++ Sources/Mvvmicro/Dependencies/Container.cs
@@ -53,6 +53,21 @@
 
         public bool IsRegistered<T>() => factories.ContainsKey(typeof(T));
 
+        public void Unregister<T>()
+        {
+            if (this.IsRegistered<T>())
+            {
+                instances[typeof(T)] = null;
+                factories[typeof(T)] = null;
+            }
+        }
+
+        public void WipeContainer()
+        {
+            this.instances = new Dictionary<Type, object>();
+            this.factories = new Dictionary<Type, Tuple<bool, Func<object>>>();
+        }
+
         public T New<T>() => (T)this.factories[typeof(T)].Item2();
 
         #endregion
