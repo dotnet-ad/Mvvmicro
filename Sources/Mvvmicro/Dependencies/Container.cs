3d9787302bbefb4d81da189e26313bd35eb69fb4
Ned Lanon <ned.lanon@outlook.com>
2018-09-02 12:20:02 +0100

    Added container for dependency management.

---
+++ Sources/Mvvmicro/Dependencies/Container.cs
@@ -0,0 +1,58 @@
+﻿namespace Mvvmicro
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Container : IContainer
+    {
+        #region Default
+
+        private static Lazy<IContainer> instance = new Lazy<IContainer>(() => new Container());
+
+        /// <summary>
+        /// Gets the default container.
+        /// </summary>
+        /// <value>The default.</value>
+        public static IContainer Default => instance.Value;
+
+        #endregion
+
+        #region Fields
+
+        private Dictionary<Type, object> instances = new Dictionary<Type, object>();
+
+        private Dictionary<Type, Tuple<bool, Func<object>>> factories = new Dictionary<Type, Tuple<bool, Func<object>>>();
+
+        #endregion
+
+        #region Methods
+
+        public T Get<T>()
+        {
+            var factory = this.factories[typeof(T)];
+
+            if (factory.Item1)
+            {
+                if (instances.TryGetValue(typeof(T), out object instance))
+                {
+                    return (T)instance;
+                }
+
+                var newInstance = factory.Item2();
+                instances[typeof(T)] = newInstance;
+                return (T)newInstance;
+            }
+
+            return (T)factory.Item2();
+        }
+
+        public void Register<T>(Func<IContainer, T> factory, bool isInstance = false)
+        {
+            this.factories[typeof(T)] = new Tuple<bool, Func<object>>(isInstance, () => factory(this));
+        }
+
+        public T New<T>() => (T)this.factories[typeof(T)].Item2();
+
+        #endregion
+    }
+}
