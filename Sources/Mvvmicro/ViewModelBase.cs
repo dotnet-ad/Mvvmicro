d5496a72ed96876677b3fd431479e90c860fd156
Ned Lanon <ned.lanon@outlook.com>
2018-09-02 12:20:02 +0100

    Added a command dedicated to navigation.

--- Sources/Mvvmicro/ViewModelBase.cs
+++ Sources/Mvvmicro/ViewModelBase.cs
@@ -1,15 +1,26 @@
 ﻿namespace Mvvmicro
 {
-	using System;
+    using System;
 
 	/// <summary>
 	/// A view model base class that adds navigation elements to the observable class.
 	/// </summary>
     public abstract class ViewModelBase : Observable, IViewModel
 	{
-		#region Navigation
+        #region Constructors
 
-		public event EventHandler<NavigationArgs> NavigationRequested;
+        public ViewModelBase()
+        {
+            this.NavigateCommand = new RelayCommand<NavigationArgs>(Navigate);
+        }
+
+        #endregion
+
+        #region Navigation
+
+        public RelayCommand<NavigationArgs> NavigateCommand { get; set; }
+
+        public event EventHandler<NavigationArgs> NavigationRequested;
 
 		public void Navigate<T>(Action<NavigationUrlSegment> prepareQuery, NavigationDirection direction = NavigationDirection.Forward) 
 		{
@@ -18,12 +29,14 @@
 			this.Navigate(new NavigationUrl(new [] { segment }), direction);
 		}
 
-		public void Navigate(string url, NavigationDirection direction = NavigationDirection.Forward) => this.Navigate(new NavigationUrl(url), direction);
+        public void Navigate(NavigationArgs args) => this.NavigateCommand?.TryExecute(args);
 
-		public void Navigate(NavigationUrl url, NavigationDirection direction = NavigationDirection.Forward) => this.NavigationRequested?.Invoke(this, new NavigationArgs(direction, url));
+        public void Navigate(string url, NavigationDirection direction = NavigationDirection.Forward) => this.Navigate(new NavigationArgs(direction,  new NavigationUrl(url)));
 
-		public void NavigateBack() => this.NavigationRequested?.Invoke(this, new NavigationArgs(NavigationDirection.Backward));
+		public void Navigate(NavigationUrl url, NavigationDirection direction = NavigationDirection.Forward) => this.Navigate(new NavigationArgs(direction, url));
 
-		#endregion
-	}
+        public void NavigateBack() => this.Navigate(new NavigationArgs(NavigationDirection.Backward));
+
+        #endregion
+    }
 }
