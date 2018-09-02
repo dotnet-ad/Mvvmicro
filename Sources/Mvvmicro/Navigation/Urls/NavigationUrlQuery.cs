754ef47364c4c43909c4309872b5dcc811886304
Ned Lanon <ned.lanon@outlook.com>
2018-09-02 12:20:02 +0100

    fixing the management of Guid on url parameters

--- Sources/Mvvmicro/Navigation/Urls/NavigationUrlQuery.cs
+++ Sources/Mvvmicro/Navigation/Urls/NavigationUrlQuery.cs
@@ -1,30 +1,30 @@
 ﻿namespace Mvvmicro
 {
-	using System;
-	using System.Collections.Generic;
-	using System.Runtime.CompilerServices;
-	using Mvvmicro.Extensions;
-	using System.Reflection;
+    using System;
+    using System.Collections.Generic;
+    using System.Runtime.CompilerServices;
+    using Mvvmicro.Extensions;
+    using System.Reflection;
 
-	/// <summary>
-	/// A query part of an url segment (ie:'?arg=4&other=5' in '/Main/Detail?arg=4&other=5').
-	/// </summary>
-	public class NavigationUrlQuery
-	{
-		#region Constructor 
+    /// <summary>
+    /// A query part of an url segment (ie:'?arg=4&other=5' in '/Main/Detail?arg=4&other=5').
+    /// </summary>
+    public class NavigationUrlQuery
+    {
+        #region Constructor 
 
-		public NavigationUrlQuery(string queryString)
-		{
-			this.String = queryString;
-		}
+        public NavigationUrlQuery(string queryString)
+        {
+            this.String = queryString;
+        }
 
-		public NavigationUrlQuery(Dictionary<string, object> parameters)
-		{
-			foreach (var pair in parameters)
-			{
-				this.parameters[pair.Key] = this.serializer.Serialize(pair.Value);
-			}
-		}
+        public NavigationUrlQuery(Dictionary<string, object> parameters)
+        {
+            foreach (var pair in parameters)
+            {
+                this.parameters[pair.Key] = this.serializer.Serialize(pair.Value);
+            }
+        }
 
         public NavigationUrlQuery(NavigationUrlQuery other)
         {
@@ -34,44 +34,44 @@
             }
         }
 
-		#endregion
+        #endregion
 
-		#region Properties
+        #region Properties
 
-		/// <summary>
-		/// Gets or sets the query as an url query string (ie: '?id=5&title=kjoidsfj').
-		/// </summary>
-		/// <value>The URL parameters.</value>
-		public string String
-		{
-			get { return parameters?.ToQueryString(); }
-			set { this.parameters = value?.ToQueryParameters() ?? new Dictionary<string, string>(); }
-		}
+        /// <summary>
+        /// Gets or sets the query as an url query string (ie: '?id=5&title=kjoidsfj').
+        /// </summary>
+        /// <value>The URL parameters.</value>
+        public string String
+        {
+            get { return parameters?.ToQueryString(); }
+            set { this.parameters = value?.ToQueryParameters() ?? new Dictionary<string, string>(); }
+        }
 
-		#endregion
+        #endregion
 
-		#region Fields
+        #region Fields
 
-		private INavigationUrlParameterSerializer serializer = new NavigationUrlParameterSerializer();
+        private INavigationUrlParameterSerializer serializer = new NavigationUrlParameterSerializer();
 
         private Dictionary<string, string> parameters = new Dictionary<string, string>();
 
-		#endregion
+        #endregion
 
-		#region Query arguments
+        #region Query arguments
 
-		/// <summary>
-		/// Set the value of the argument with the given key.
-		/// </summary>
-		/// <returns>The set.</returns>
-		/// <param name="value">Value.</param>
-		/// <param name="key">Key.</param>
-		/// <typeparam name="T">The 1st type parameter.</typeparam>
-		public NavigationUrlQuery Set<T>(T value, [CallerMemberName] string key = null)
-		{
-			parameters[key] = serializer.Serialize(value);
-			return this;
-		}
+        /// <summary>
+        /// Set the value of the argument with the given key.
+        /// </summary>
+        /// <returns>The set.</returns>
+        /// <param name="value">Value.</param>
+        /// <param name="key">Key.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        public NavigationUrlQuery Set<T>(T value, [CallerMemberName] string key = null)
+        {
+            parameters[key] = serializer.Serialize(value);
+            return this;
+        }
 
         /// <summary>
         /// Get the value of the argument with the given key.
@@ -85,7 +85,7 @@
         {
             if (this.TryGet(t, out object result, key))
                 return result;
-            
+
             return Activator.CreateInstance(t);
         }
 
@@ -108,30 +108,36 @@
 
         }
 
-		/// <summary>
-		/// Try to get the value of the argument with the given key.
-		/// </summary>
-		/// <returns>The get.</returns>
-		/// <param name="key">Key.</param>
-		public bool TryGet(Type t, out object result, [CallerMemberName] string key = null)
-		{
-			string stringValue;
-			if(parameters.TryGetValue(key, out stringValue))
-			{
+        /// <summary>
+        /// Try to get the value of the argument with the given key.
+        /// </summary>
+        /// <returns>The get.</returns>
+        /// <param name="key">Key.</param>
+        public bool TryGet(Type t, out object result, [CallerMemberName] string key = null)
+        {
+            string stringValue;
+            if (parameters.TryGetValue(key, out stringValue))
+            {
                 try
                 {
+                    if (t == typeof(Guid))
+                    {
+                        result = new Guid(stringValue);
+                        return true;
+                    }
+
                     result = serializer.Deserialize(stringValue, t);
                     return true;
                 }
-                catch (Exception) {}
-			}
+                catch (Exception) { }
+            }
 
             result = null;
-			return false;
+            return false;
         }
 
-		#endregion
+        #endregion
 
-		public override string ToString() => this.String;
-	}
+        public override string ToString() => this.String;
+    }
 }
